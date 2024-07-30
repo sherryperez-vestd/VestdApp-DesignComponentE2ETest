e51cb2d360139911f1fc4de08e24fda6fd854431
sherryperez-vestd <sherry.perez@vestd.com>
2024-07-30 09:11:47 +0100

    30-7-2024

--- pages/form-number-input.ts
+++ pages/form-number-input.ts
@@ -6,11 +6,11 @@ export default class formNumberInput {
     private base: BaseFunctions
 
     readonly allowDecimalValueInput: Locator;
-    readonly FormatTheNumberInput: Locator;
+    readonly formatTheNumberInput: Locator;
     
     constructor(page: Page) {
         this.allowDecimalValueInput = page.getByLabel('Test4', { exact: true })
-        this.FormatTheNumberInput = page.getByLabel('Test2', { exact: true })
+        this.formatTheNumberInput = page.getByLabel('Test2', { exact: true })
         
     }
 
--- tests/form-number-input.test.ts
+++ tests/form-number-input.test.ts
@@ -26,14 +26,14 @@ test("Format the number", async ({ assertions, functions, formNumberInput }) => 
     await functions.goto("/design/forms-number")
     await assertions.verifyURLContains("forms-number")
 
-    await assertions.verifyElementisVisible(formNumberInput.FormatTheNumberInput)
-    await formNumberInput.FormatTheNumberInput.clear()
-    await formNumberInput.FormatTheNumberInput.type("1111111111111")
-    await assertions.verifyElementToHaveValue(formNumberInput.FormatTheNumberInput, "1,111,111,111,111")    
+    await assertions.verifyElementisVisible(formNumberInput.formatTheNumberInput)
+    await formNumberInput.formatTheNumberInput.clear()
+    await formNumberInput.formatTheNumberInput.type("1111111111111")
+    await assertions.verifyElementToHaveValue(formNumberInput.formatTheNumberInput, "1,111,111,111,111")    
 
-    await formNumberInput.FormatTheNumberInput.clear()
-    await formNumberInput.FormatTheNumberInput.type("0")
-    await assertions.verifyElementToHaveValue(formNumberInput.FormatTheNumberInput, "0")
+    await formNumberInput.formatTheNumberInput.clear()
+    await formNumberInput.formatTheNumberInput.type("0")
+    await assertions.verifyElementToHaveValue(formNumberInput.formatTheNumberInput, "0")
 })
 
 
